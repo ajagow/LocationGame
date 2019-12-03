cdba69adbd2e7ad9edc17d2cf693291f205dd274
ajagow <budgiejag@gmail.com>
2019-12-03 14:06:43 -0500

    added button markers

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -8,34 +8,93 @@ import {
   AlertIOS
 } from "react-native";
 import MapView, { Marker, AnimatedRegion } from "react-native-maps";
+import { isPointWithinRadius } from "geolib";
 
 const screen = Dimensions.get("window");
 
 const ASPECT_RATIO = screen.width / screen.height;
 const LATITUDE_DELTA = 0.0922;
 const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const RADIUS = 200;
 
 export default class MapScreen extends Component {
   constructor() {
     super();
     this.state = {
+      position: null,
+      error: null,
+      isWithinPointRadius: false,
       members: [
         {
           id: "6jmItzqXbT:rT9M2uNHxK", // unique user ID assigned by Scaledrone
           authData: {
             // authData is created by the JWT sent from the authentication server
             color: "#f032e6", // unique color hash from the authentication server
-            name: "John" // user is prompted to insert their name
+            name: "pencil" // user is prompted to insert their name
           },
           location: {
-            latitude: 42.338142559575566,
-            longitude: -71.093457360938,
+            latitude: 43.33673132323687,
+            longitude: -71.09021056333766,
+            latitudeDelta: 0.5,
+            longitudeDelta: 0.5
+          } // react-native-maps marker location
+        },
+        {
+          id: "6jmItzqXbT:rT9M2uNHxL", // unique user ID assigned by Scaledrone
+          authData: {
+            // authData is created by the JWT sent from the authentication server
+            color: "#f032e6", // unique color hash from the authentication server
+            name: "golden apple" // user is prompted to insert their name
+          },
+          location: {
+            latitude: 42.3429,
+            longitude: -71.0858,
             latitudeDelta: 0.5,
             longitudeDelta: 0.5
           } // react-native-maps marker location
         }
       ]
     };
+  }
+
+  isUserInRadius(position) {
+    const { members } = this.state;
+    let isOneInRange = false;
+    members.forEach(member => {
+      const { id, location, authData } = member;
+      const isPoint = isPointWithinRadius(
+        {
+          latitude: position["coords"].latitude,
+          longitude: position["coords"].longitude
+        },
+        { latitude: location.latitude, longitude: location.longitude },
+        RADIUS
+      );
+
+      if (isPoint) {
+        isOneInRange = true;
+      }
+    });
+
+    if (isOneInRange) {
+      this.setState({ isWithinPointRadius: true });
+    }
+  }
+
+  componentDidMount() {
+    navigator.geolocation.watchPosition(
+      position => {
+        this.setState({
+          position: {
+            longitude: position["coords"].longitude,
+            latitude: position["coords"].latitude
+          }
+        });
+        this.isUserInRadius(position);
+      },
+      error => this.setState({ error: error.message }),
+      { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 }
+    );
   }
 
   createMarkers() {
@@ -45,15 +104,38 @@ export default class MapScreen extends Component {
       const { id, location, authData } = member;
       const { name, color } = authData;
       return (
-        <Marker.Animated
-          key={id}
-          identifier={id}
-          coordinate={location}
-          pinColor={color}
-          title={name}
-        />
+        <View>
+          <Marker.Animated
+            key={id}
+            identifier={id}
+            coordinate={location}
+            pinColor={color}
+            title={name}
+          />
+          <MapView.Circle
+            key={id}
+            center={location}
+            radius={RADIUS}
+            strokeWidth={2}
+            strokeColor="#3399ff"
+            fillColor="rgba(51,153,255,.5)"
+          />
+        </View>
       );
     });
+  }
+
+  createYou() {
+    const { position } = this.state;
+    if (position) {
+      return (
+        <MapView.Marker
+          coordinate={this.state.position}
+          title="you"
+          description="This is where you are"
+        />
+      );
+    }
   }
 
   createMembers() {
@@ -74,7 +156,20 @@ export default class MapScreen extends Component {
     this.map.fitToSuppliedMarkers(members.map(m => m.id), true);
   }
 
+  fitMeToMap() {
+    console.log("dkfjlsdkf" + this.state.position);
+    let something = {
+      ...this.state.position,
+      ...{ latitudeDelta: 0.001, longitudeDelta: 0.001 }
+    };
+    this.map.animateToRegion(something, 100);
+  }
+
   render() {
+    const { position, isWithinPointRadius } = this.state;
+    if (!position) {
+      return <View style={styles.container}></View>;
+    }
     return (
       <View style={styles.container}>
         <MapView
@@ -83,25 +178,44 @@ export default class MapScreen extends Component {
             this.map = ref;
           }}
           initialRegion={{
-            latitude: 37.600425,
-            longitude: -122.385861,
+            latitude: position.latitude,
+            longitude: position.longitude,
             latitudeDelta: LATITUDE_DELTA,
             longitudeDelta: LONGITUDE_DELTA
           }}
         >
           {this.createMarkers()}
+          {this.createYou()}
         </MapView>
         <View pointerEvents="none" style={styles.members}>
           {this.createMembers()}
         </View>
-        <View style={styles.buttonContainer}>
+        <View style={[styles.buttonContainer, { marginBottom: 20 }]}>
           <TouchableOpacity
             onPress={() => this.fitToMarkersToMap()}
             style={[styles.bubble, styles.button]}
           >
-            <Text>Fit Markers Onto Map</Text>
+            <Text>Find all Markers</Text>
           </TouchableOpacity>
         </View>
+        <View style={[styles.buttonContainer, { marginBottom: 10 }]}>
+          <TouchableOpacity
+            onPress={() => this.fitMeToMap()}
+            style={[styles.bubble, styles.button]}
+          >
+            <Text>Find Me</Text>
+          </TouchableOpacity>
+        </View>
+        {isWithinPointRadius && (
+          <View style={[styles.buttonContainer, { marginBottom: 200 }]}>
+            <TouchableOpacity
+              onPress={() => console.warn("you are within radius")}
+              style={[styles.bubble, styles.button]}
+            >
+              <Text>You are within radius</Text>
+            </TouchableOpacity>
+          </View>
+        )}
       </View>
     );
   }
